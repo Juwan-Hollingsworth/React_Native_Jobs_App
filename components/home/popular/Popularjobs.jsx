57c698c8a28aa4278411499a088196911efc9984
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-02-22 17:29:17 -0500

    implement loading data logic to component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native'
+import { useState } from 'react'
+import { useRouter } from 'expo-router'
 import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants'
+import PopularjobCard from '../../common/cards/popular/PopularJobCard'
+import { isLoaded } from 'expo-font'
 
 const Popularjobs = () => {
+  const router = useRouter()
+  //loading spinner toggle
+  const isLoading = false
+  const error = false
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      {/* where our jobs will be held */}
+      <View style={styles.cardsContainer}>
+        {/* check if data is still loading - if so, show loading spinner if not display flatlist or error*/}
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList />
+        )}
+      </View>
     </View>
   )
 }
