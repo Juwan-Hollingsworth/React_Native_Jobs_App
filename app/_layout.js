7ae02651706fd6b0d950b4f5e52332b5056a8659
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-02-21 23:48:31 -0500

    package installs and code revision

--- app/_layout.js
+++ app/_layout.js
@@ -1,27 +1,28 @@
-
-import {Stack} from 'expo-router';
-import { useCallback } from 'react';
-import {useFonts} from 'expo-font';
-import * as SplashScreen from "expo-splash-screen"
+import { Stack } from 'expo-router'
+import { useCallback } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
 
 //Prevent splash screen from hiding automatically
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
-    //Load fonts using useFonts hook
-    const [fontsLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.tff'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.tff'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.tff')
-    })
-    //define callback fx to be invoked when layout changes 
-    const onLayoutRootView = useCallback(async () => {
-        //if fonts are loaded, hide them behind the splash screen
-        if(fontsLoaded){
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded])
-    return <Stack/>;
+  //Load fonts using useFonts hook
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  })
+  //define callback fx to be invoked when layout changes
+  const onLayoutRootView = useCallback(async () => {
+    //if fonts are loaded, hide them behind the splash screen
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) return null
+  return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout
