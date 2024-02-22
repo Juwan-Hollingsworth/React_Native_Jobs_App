d60bd3fa84c77c2ddb07d035bf657b68a813e1a5
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-02-21 23:47:32 -0500

    Add responsive elements to Welcome component

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,34 +1,63 @@
 import React from 'react'
-import { 
+import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
   Image,
-  FlatList
-  } from 'react-native'
+  FlatList,
+} from 'react-native'
 import { useState } from 'react'
-import {useRouter} from 'expo-router'
+import { useRouter } from 'expo-router'
 import styles from './welcome.style'
-import {icons, SIZES} from "../../../constants"
+import { icons, SIZES } from '../../../constants'
+
+// Define different types of job in an array
+const jobTypes = ['Full-time', 'Part-time', 'Contractor']
 
 const Welcome = () => {
   //define router
-  const router = useRouter();
+  const router = useRouter()
   return (
     <View>
-    {/* First inner View component */}
-    <View style={styles.container}>
-    <Text style={styles.userName}>Hello Juwan</Text>
-    <Text style={styles.welcomeMessage}>Find your perfect job</Text>
-    </View>
-    {/* Second inner View component */}
-    <View style={styles.searchContainer}>
-    <View style={styles.searchWrapper}>
-      <TextInput style={styles.searchInput}/>
-    </View>
+      {/* First inner View component */}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Juwan</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      {/* Second inner View component */}
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          {/* Input field for searching */}
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        {/* Responsive component containing an image for search button */}
 
-    </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      {/* Display job types using a FlatList */}
+      {/* Each job type is a touchable element */}
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   )
 }
