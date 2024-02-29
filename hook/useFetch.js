d204c86a76e512908df07bee5beb7bc516b1d88a
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-02-29 17:18:30 -0500

    implement fetch data function

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import { RAPID_API_KEY } from '@env'
-const rapidAPIKey = RAPID_API_KEY
+const rapidAPIKey = 'ea7266ed7bmsh361b7f31e9185bep104572jsnfec9256392cb'
 
 //useFetch will utilize the endpoint prop to fetch data
 const useFetch = (endpoint, query) => {
@@ -11,6 +10,7 @@ const useFetch = (endpoint, query) => {
   const [error, setError] = useState(null)
 
   //template string that take the prop and alters the api call
+  //configure options for the API request
   const options = {
     method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
@@ -20,28 +20,34 @@ const useFetch = (endpoint, query) => {
     },
     params: { ...query },
   }
-
+  //fetch data from the API
   const fetchData = async () => {
     setIsLoading(true)
 
     try {
+      //try to make a API request using Axios
       const response = await axios.request(options)
       setData(response.data.data)
-      isLoading(false)
+      setIsLoading(false)
     } catch (error) {
       setError(error)
-      alert('There is an error')
+      alert('There is an error in the request')
     } finally {
       setIsLoading(false)
     }
   }
-
+  //useEffect hook to trigger the fetchData when the component mounts
   useEffect(() => {
     fetchData()
   }, [])
-
+  //function to manually refetch data
   const refetch = () => {
     setIsLoading(true)
     fetchData()
   }
+
+  //return the state variables and refresh functions
+  return { data, isLoading, error, refetch }
 }
+
+export default useFetch
