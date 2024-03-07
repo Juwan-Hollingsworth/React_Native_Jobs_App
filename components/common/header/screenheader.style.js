61b3670c741140845f07f9daaea82b368ced2a72
Juwan Hollingsworth <jhollingsworth@domain.local>
2024-03-07 00:04:28 -0500

    update user image

--- components/common/header/screenheader.style.js
+++ components/common/header/screenheader.style.js
@@ -1,21 +1,22 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
-import { COLORS, SIZES } from "../../../constants";
+import { COLORS, SIZES } from '../../../constants'
 
 const styles = StyleSheet.create({
   btnContainer: {
-    width: 40,
-    height: 40,
+    width: 60,
+    height: 50,
     backgroundColor: COLORS.white,
     borderRadius: SIZES.small / 1.25,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingRight: 16,
   },
   btnImg: (dimension) => ({
     width: dimension,
     height: dimension,
     borderRadius: SIZES.small / 1.25,
   }),
-});
+})
 
-export default styles;
+export default styles
